a7b61d6d33b44518cbd461fcc3277a6af3bdfc4e
0xed123 <apollo1030109@gmail.com>
2023-03-16 04:13:04 +0900

    update team wallet

--- jackport-frontend/src/context/solana/types.ts
+++ jackport-frontend/src/context/solana/types.ts
@@ -12,7 +12,7 @@ export const CODY_WALLET  = new PublicKey("GBfam19CeWi6msbrgHxFPmEBZMu4zHavQyNch
 export const BET3_WALLET  = new PublicKey("41m5znXJg9CkxKBocJxntJZKn59U3BomkafEguxDDEWG");   // 1.08%
 export const JERZY_WALLET  = new PublicKey("79Zzb6b6JwxSc6SxwDJ8XuVci2hz45XwLQgi1aQBnMYX");  // 0.72%
 export const EXPER_WALLET  = new PublicKey("5dxyb7RWSdw1o9VXBN1gfL9oViBubeERx9g2HY74AHyD");  // 1.08%
-export const TEAM_WALLET  = new PublicKey("32NL69SFk8GLPFZfKQwsuexcXHd7rqAQn1mrasF1ksVj");   // 0.4%export
+export const TEAM_WALLET  = new PublicKey("8Gbqb5ppmsocN8JMGBLNUHdn9zoZuiA6qzgwEPgN6j71");   // 0.4%export
 export interface GamePool {
   startTs: anchor.BN,
   rand: anchor.BN,
