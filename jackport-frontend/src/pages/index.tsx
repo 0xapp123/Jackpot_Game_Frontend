ddc76141bf101bd61331f63673c391dd84e5d687
dev-tom-0108 <shinji.sakai111@gmail.com>
2023-03-17 02:12:49 +0900

    fix: fix index.tsx props

--- jackport-frontend/src/pages/index.tsx
+++ jackport-frontend/src/pages/index.tsx
@@ -1,15 +1,18 @@
 import Head from 'next/head'
 import Home from './home'
-export default function Index() {
-  return (
-    <>
-      <Head>
-        <title>SlowRUG</title>
-        <meta name="description" content="SlowRUG | Best Crypto PvP Gambling Website" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Home/>
-    </>
-  )
+export default function Index(props: {
+ isMute: boolean,
+ setIsMute: Function
+}) {
+ return (
+  <>
+   <Head>
+    <title>SlowRUG</title>
+    <meta name="description" content="SlowRUG | Best Crypto PvP Gambling Website" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="/favicon.ico" />
+   </Head>
+   <Home isMute={props.isMute} setIsMute={props.setIsMute} />
+  </>
+ )
 }
