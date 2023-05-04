50a2eb7ba3a8694e163d5441a252399ae3befc4d
0xed123 <apollo1030109@gmail.com>
2023-05-05 00:04:12 +0900

    fix countdown

--- jackport-frontend/src/components/CountdownBarInfinite.tsx
+++ jackport-frontend/src/components/CountdownBarInfinite.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
-import { useSocket } from "../context/SocketContext";
-import Sound from "react-sound";
+import { useSocket } from "../context/SocketContextInfinite";
 import { FIRST_COOLDOWN } from "../config";
 
 export default function CountdownBar(props: {
@@ -81,11 +80,11 @@ export default function CountdownBar(props: {
       className={`${className ? className : ""
         } w-[calc(100%-60px)] mx-[30px] mt-[150px] absolute text-center`}
     >
-      <p className="text-white font-semibold ">Countdown</p>
+      <p className="font-semibold text-white ">Countdown</p>
       <div className="absolute w-full bg-[#050d36] h-2 rounded-3xl mt-4">
         {timeRemaining}
         {/* {gameData && (gameData.endTimestamp <= 0 || gameData.endTimestamp < new Date().getTime()) &&
-                    <div className="absolute bg-blue-600 h-2 rounded-3xl"></div>
+                    <div className="absolute h-2 bg-blue-600 rounded-3xl"></div>
                 } */}
       </div>
       {/* <Sound
